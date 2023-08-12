86a5eb8c841b4fda3146ed21fa82140e5ffd5068
MTahaSh <tahashayk2k@gmail.com>
2023-08-12 11:40:21 +0500

    Update route.ts

--- app/api/auth/web2logout/route.ts
+++ app/api/auth/web2logout/route.ts
@@ -8,10 +8,12 @@ export async function GET()
             success: true
         })
 
-        response.cookies.set("token","",{
-            httpOnly:true,
-            expires: new Date(0)
-        });
+        response.cookies.delete("token");
+        
+        // response.cookies.set("token"," ",{
+        //     httpOnly:true,
+        //     expires: new Date(0)
+        // });
 
         return response;
     } catch (error:any) {
